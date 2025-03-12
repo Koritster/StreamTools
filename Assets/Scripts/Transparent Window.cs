7de9bc3139ea3bb3bf7d629c80661541b1be0f2f
Koritster <64407378+Koritster@users.noreply.github.com>
2025-03-12 13:46:57 -0600

    Grab avatar

--- Assets/Scripts/Transparent Window.cs
+++ Assets/Scripts/Transparent Window.cs
@@ -8,7 +8,10 @@ public class TransparentWindow : MonoBehaviour
     private const int WS_EX_TRANSPARENT = 0x00000020;
     private const int GWL_EXSTYLE = -20;
     private const uint LWA_COLORKEY = 0x00000001;
-    private const uint LWA_ALPHA = 0x00000002;
+    private const int HWND_TOPMOST = -1;
+    private const int SWP_NOSIZE = 0x0001;
+    private const int SWP_NOMOVE = 0x0002;
+    private const int SWP_SHOWWINDOW = 0x0040;
 
     [DllImport("user32.dll")]
     private static extern IntPtr GetActiveWindow();
@@ -22,15 +25,26 @@ public class TransparentWindow : MonoBehaviour
     [DllImport("user32.dll", SetLastError = true)]
     private static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
 
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    private IntPtr hWnd;
+    private int originalStyle;
+
     private void Start()
     {
-        IntPtr hWnd = GetActiveWindow();
+
+#if !UNITY_EDITOR
+        hWnd = GetActiveWindow();
 
         // Activar el estilo de ventana en modo transparente
         int style = GetWindowLong(hWnd, GWL_EXSTYLE);
-        SetWindowLong(hWnd, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        SetWindowLong(hWnd, GWL_EXSTYLE, style | WS_EX_LAYERED);
 
         // Hacer el color negro transparente (puedes cambiarlo si necesitas otro color)
         SetLayeredWindowAttributes(hWnd, 0, 255, LWA_COLORKEY);
+
+        SetWindowPos(hWnd, (IntPtr)HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW);
+#endif
     }
 }
